3bf8fce921f85a177e7a9dc7b6ebf70340198539
guillaume <guillaume.duhamel@edukera.com>
2014-05-14 10:20:50 +0200

    up

--- Website/src/main/java/com/edukera/website/client/main/view/ContainerView.java
+++ Website/src/main/java/com/edukera/website/client/main/view/ContainerView.java
@@ -2,22 +2,21 @@ package com.edukera.website.client.main.view;
 
 import com.edukera.website.client.generic.view.ADivView;
 import com.edukera.website.client.generic.widget.DivPanel;
-import com.edukera.website.client.generic.widget.ImgPanel;
 import com.edukera.website.client.main.presenter.Container;
 import com.google.gwt.user.client.ui.Widget;
 
 public class ContainerView extends ADivView implements Container.Display {
 
-	private ImgPanel mImg;
+//	private ImgPanel mImg;
 	private final DivPanel mWrapper;
 	
 	public ContainerView() {
 		mRoot.addStyleName(STYLE.container());
 		
-		mImg = new ImgPanel();
-		mRoot.add(mImg);
-		mImg.addStyleName(STYLE.containerImg());
-		mImg.getElement().setAttribute("src", "../style/img/sky.jpg");
+//		mImg = new ImgPanel();
+//		mRoot.add(mImg);
+//		mImg.addStyleName(STYLE.containerImg());
+//		mImg.getElement().setAttribute("src", "../style/img/sky.jpg");
 		
 		mWrapper = new DivPanel();
 		mRoot.add(mWrapper);
