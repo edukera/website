ef06209662036701ca10af2415e09e576e72ff32
guillaume <guillaume.duhamel@edukera.com>
2014-05-15 22:57:25 +0200

    up

--- Website/src/main/java/com/edukera/website/client/main/presenter/impl/MainImpl.java
+++ Website/src/main/java/com/edukera/website/client/main/presenter/impl/MainImpl.java
@@ -12,6 +12,8 @@ import com.edukera.website.client.main.presenter.ContainerSky;
 import com.edukera.website.client.main.presenter.Footer;
 import com.edukera.website.client.main.presenter.Header;
 import com.edukera.website.client.main.presenter.Main;
+import com.google.gwt.event.logical.shared.ResizeEvent;
+import com.google.gwt.event.logical.shared.ResizeHandler;
 import com.google.gwt.user.client.Window;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
@@ -24,7 +26,7 @@ public class MainImpl extends ADrawImpl<Main.Display> implements Main {
 	private final ContainerFeature mContainerFeature;
 	private final ContainerHilbert mContainerHilbert;
 	private final Footer mFooter;
-	
+
 	@Inject
 	public MainImpl(EventBus eventBus, Main.Display display) {
 		super(eventBus, display);
@@ -33,14 +35,14 @@ public class MainImpl extends ADrawImpl<Main.Display> implements Main {
 		mContainerFeature = EdukeraWebsite.ginjector.getContainerFeature();
 		mContainerHilbert = EdukeraWebsite.ginjector.getContainerHilbert();
 		mFooter = EdukeraWebsite.ginjector.getFooter();
-		
+
 		display.add(mHeader.getDisplay().asWidget());
 		display.add(mContainerSky.getDisplay().asWidget());
 		display.add(mContainerFeature.getDisplay().asWidget());
 		display.add(mContainerHilbert.getDisplay().asWidget());
 		display.add(mFooter.getDisplay().asWidget());
 	}
-	
+
 	@Override
 	public void bind() {
 		super.bind();
@@ -49,15 +51,24 @@ public class MainImpl extends ADrawImpl<Main.Display> implements Main {
 		mContainerFeature.bind();
 		mContainerHilbert.bind();
 		mFooter.bind();
+
+		registerHandler(Window.addResizeHandler(new ResizeHandler() {
+
+			@Override
+			public void onResize(ResizeEvent event) {
+				draw();
+			}
+		}));
+		
 	}
 
 	@Override
 	public void draw() {
 		int lHeight = Window.getClientHeight();
 		display.setHeight(lHeight);
-		
+
 		DataResources.getInstance().switchLanguage(mLanguage);
-		
+
 		mHeader.draw();
 		mContainerSky.draw();
 		mContainerFeature.draw();
@@ -82,12 +93,12 @@ public class MainImpl extends ADrawImpl<Main.Display> implements Main {
 	public void scrollAbout() {
 		scroll(mContainerHilbert);
 	}
-	
+
 	private void scroll(ADraw<?> iComponent) {
 		int lBegin = display.getElement().getScrollTop();
 		int lValue = iComponent.getDisplay().getElement().getAbsoluteTop();
 		Scroller lScroller = new Scroller(display, lBegin, lValue);
 		lScroller.run(300);
 	}
-	
+
 }
