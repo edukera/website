86a71c06d71387e8e70ee6b2d0b6546281d07c76
guillaume <guillaume.duhamel@edukera.com>
2014-05-19 17:44:05 +0200

    up

--- Website/src/main/java/com/edukera/website/client/logo/view/LogoCalculusView.java
+++ Website/src/main/java/com/edukera/website/client/logo/view/LogoCalculusView.java
@@ -6,17 +6,11 @@ import com.edukera.website.client.logo.presenter.LogoCalculus;
 
 public class LogoCalculusView extends ADivView implements LogoCalculus.Display {
 
-	private final DivPanel mLogo;
 	private final DivPanel mOperators;
 	
 	public LogoCalculusView() {
 		mRoot.addStyleName(STYLE.logoCalculus());
 		
-		mLogo = new DivPanel();
-		mRoot.add(mLogo);
-		mLogo.addStyleName(STYLE.logoCalculusLogo());
-		mLogo.setText("(");
-
 		mOperators = new DivPanel();
 		mRoot.add(mOperators);
 		mOperators.addStyleName(STYLE.logoCalculusOperator());
