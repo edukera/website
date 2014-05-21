20b5d16e2b125d4c49602b74c191cdc13ffbed8b
guillaume <guillaume.duhamel@edukera.com>
2014-05-21 11:06:57 +0200

    up

--- Website/src/main/java/com/edukera/website/client/main/view/ContainerAboutView.java
+++ Website/src/main/java/com/edukera/website/client/main/view/ContainerAboutView.java
@@ -3,6 +3,7 @@ package com.edukera.website.client.main.view;
 import com.edukera.website.client.generic.view.ADivView;
 import com.edukera.website.client.generic.widget.DivPanel;
 import com.edukera.website.client.main.presenter.ContainerAbout;
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.user.client.ui.Widget;
 
 public class ContainerAboutView extends ADivView implements ContainerAbout.Display {
@@ -19,5 +20,10 @@ public class ContainerAboutView extends ADivView implements ContainerAbout.Displ
 	
 	public void addWrapper(Widget iWidget) {
 		mWrapper.add(iWidget);
+	}
+	
+	public void setHeight(double iHeight) {
+		super.setHeight(iHeight);
+		mWrapper.getElement().getStyle().setHeight(iHeight, Unit.PX);
 	}
 }
