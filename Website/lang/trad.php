542089b36b6b5a0f7f64645d29873a3446393a7d
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2015-06-18 06:21:42 +0200

    switch website to default language when "lang" property is not set

--- Website/lang/trad.php
+++ Website/lang/trad.php
@@ -1,23 +1,32 @@
 <?php
-if(empty($_GET['lang'])){
-    $_SESSION['lang'] = "fr";
-}else{
-    switch($_GET['lang']){
-        case "fr":
+
+if (empty($_GET['lang'])) {
+    $language = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+    switch ($language){
+    case "fr":
         $_SESSION['lang'] = "fr";
         break;
-        case "en":
+    default:
+        $_SESSION['lang'] = "en";
+        break;
+    }
+}else{
+    switch($_GET['lang']){
+    case "fr":
+        $_SESSION['lang'] = "fr";
+        break;
+    case "en":
         $_SESSION['lang'] = "en";
         break;
     }
 }
 switch($_SESSION['lang']){
-        case "fr":
-        $fichier_langage = "lang/fichier_fr.php";
-        break;
-        case "en":
-        $fichier_langage = "lang/fichier_en.php";
-        break;
+case "fr":
+    $fichier_langage = "lang/fichier_fr.php";
+    break;
+case "en":
+    $fichier_langage = "lang/fichier_en.php";
+    break;
 }
 include($fichier_langage);
 $lang = $_SESSION['lang'];
